f1f4a5951601d8771b56b71e5a3990a08a3d735b
kanil93 <kanil93@wp.pl>
2015-12-03 23:49:46 +0100

    Update ProjektZespolowy.java

--- src/projektzespolowy/ProjektZespolowy.java
+++ src/projektzespolowy/ProjektZespolowy.java
@@ -8,8 +8,9 @@ public class ProjektZespolowy {
     public static void main(String[] args) {
         int a;
         System.out.println("To jest zajebisty program");
-        System.out.println("To jest zajebisty program");
+        //System.out.println("To jest zajebisty program");
         System.out.println("czemu to jest dwa razy???");
+        System.out.println("żeby bylo wiecej linijek programu");
     }
     
 }
